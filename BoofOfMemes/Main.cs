2a687a65417ca17692aa8b4831335bcf48b465aa
MOPSKATER <josua.w@gmx.de>
2023-07-06 21:06:51 +0200

    Using new anticheat

--- BoofOfMemes/Main.cs
+++ BoofOfMemes/Main.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿using AntiCheat;
+using HarmonyLib;
 using MelonLoader;
 using System.Reflection;
 using UnityEngine.SceneManagement;
@@ -7,10 +8,10 @@ namespace BoofOfMemes
 {
     public class Main : MelonMod
     {
-        [Obsolete]
         public override void OnApplicationLateStart()
         {
-            GameDataManager.powerPrefs.dontUploadToLeaderboard = true;
+            Anticheat.TriggerAnticheat();
+            Anticheat.Register("BoofOfMemes");
             PatchGame();
 
             Game game = Singleton<Game>.Instance;
@@ -41,51 +42,13 @@ namespace BoofOfMemes
         {
             HarmonyLib.Harmony harmony = new("de.MOPSKATER.BoofOfMemes");
 
-            MethodInfo target = typeof(LevelStats).GetMethod("UpdateTimeMicroseconds");
-            HarmonyMethod patch = new(typeof(Main).GetMethod("PreventNewScore"));
-            harmony.Patch(target, patch);
-
-            target = typeof(Game).GetMethod("OnLevelWin");
-            patch = new(typeof(Main).GetMethod("PreventNewGhost"));
-            harmony.Patch(target, patch);
-
-            target = typeof(LevelRush).GetMethod("IsCurrentLevelRushScoreBetter", BindingFlags.NonPublic | BindingFlags.Static);
-            patch = new(typeof(Main).GetMethod("PreventNewBestLevelRush"));
-            harmony.Patch(target, patch);
-
-            target = typeof(LevelGate).GetMethod("SetUnlocked");
-            patch = new(typeof(Main).GetMethod("UnlockGate"));
+            MethodInfo target = typeof(LevelGate).GetMethod("SetUnlocked");
+            HarmonyMethod patch = new(typeof(Main).GetMethod("UnlockGate"));
             harmony.Patch(target, patch);
 
             target = typeof(MenuScreenLevelRushComplete).GetMethod("OnSetVisible");
             patch = new(typeof(Main).GetMethod("PostOnSetVisible"));
             harmony.Patch(target, null, patch);
-        }
-
-        public static bool PreventNewScore(LevelStats __instance, ref long newTime)
-        {
-            if (newTime < __instance._timeBestMicroseconds)
-            {
-                if (__instance._timeBestMicroseconds == 999999999999L)
-                    __instance._timeBestMicroseconds = 600000000;
-                __instance._newBest = true;
-            }
-            else
-                __instance._newBest = false;
-            __instance._timeLastMicroseconds = newTime;
-            return false;
-        }
-
-        public static bool PreventNewGhost(Game __instance)
-        {
-            __instance.winAction = null;
-            return true;
-        }
-
-        public static bool PreventNewBestLevelRush(ref bool __result)
-        {
-            __result = false;
-            return false;
         }
 
         public static bool UnlockGate(LevelGate __instance, ref bool u)
